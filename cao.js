d95f2ea108893e27d1acf869a07453fb11116326
Rein F <public@reinfernhout.xyz>
2019-11-14 21:17:42 +0100

    Fixed selection bug, added error logs and clearing the selection menu

--- cao.js
+++ cao.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 
 const {exec} = require("child_process")
+const fs = require("fs")
+const err_wstream = fs.createWriteStream("/tmp/cao.stderr");
 
 //Get sink list
 exec("pactl list short sinks", (err, stdout) => {
-
+	if (err) {
+		console.log("An error occured retrieving sink list")
+		err_wstream.write(err + "\n")
+		process.exit(1)
+	}
 
 	const outputLines = stdout.split("\n")
 
@@ -21,7 +27,8 @@ exec("pactl list short sinks", (err, stdout) => {
 		sinkList.push({index, name})
 	})
 
-	console.log("Please select:")
+	//Node outputs in bold by default, thus the reset char at the end
+	console.log("Please select:\033[0m")
 
 	let selected = 0;
 
@@ -30,7 +37,7 @@ exec("pactl list short sinks", (err, stdout) => {
 	function draw() {
 		
 		const display = sinkList.map(
-			sink => `${sink.index == selected ? "\033[32m" : "\033[0m"}[${sink.index}] ${sink.name}`
+			sink => `${sinkList.indexOf(sink) == selected ? "\033[32m" : "\033[0m"}[${sink.index}] ${sink.name}`
 		).join("\n")
 		
 		process.stdout.write(display)
@@ -49,12 +56,15 @@ exec("pactl list short sinks", (err, stdout) => {
 	process.stdin.on("keypress", (str, key) => {
 		if (key.ctrl && key.name == "c") {
 
-			//Set the cursor to right place
-			process.stdout.moveCursor(0, sinkList.length)
-			process.stdout.clearLine()
+			//Move 1 up to cover the "Please select" message
+			process.stdout.moveCursor(0, -1)
+			//Clear selection menu
+			process.stdout.clearScreenDown()
 
 			//Reset color
 			process.stdout.write("\033[0m")
+
+			console.log("Selection cancelled by user")
 
 			//Exit process
 			process.exit()
@@ -66,7 +76,21 @@ exec("pactl list short sinks", (err, stdout) => {
 			//stop listening for input
 			process.stdin.pause()
 			
-			route_input()
+			//Move 1 up to cover the "Please select" message
+			process.stdout.moveCursor(0, -1)
+			//Clear selection menu
+			process.stdout.clearScreenDown()
+			
+			//Reset color
+			process.stdout.write("\033[0m")
+
+			const index = sinkList[selected].index;
+
+			route_input(index)
+			set_default(index)
+
+			//Return so we don't draw again
+			return
 		}
 
 		switch (key.name) {
@@ -85,34 +109,45 @@ exec("pactl list short sinks", (err, stdout) => {
 		
 	})
 
-	const route_input = () => exec("pactl list short sink-inputs", (err, stdout) => {
-		const outputLines = stdout.split("\n")
-	
-		//remove last empty line
-		outputLines.splice(-1, 1)
-	
-		const inputList = []
-		outputLines.forEach(line => {
-			const columns = line.split("\t")
-	
-			const id = columns[0]	
-			inputList.push(id)
-		})
-
-		inputList.forEach(id => {
-			exec(`pactl move-sink-input ${id} ${selected}`)
-		})
-
-
-		//Set the cursor to right place
-		process.stdout.moveCursor(0, sinkList.length)
-		process.stdout.clearLine()
-
-		//Reset color
-		process.stdout.write("\033[0m")
-
-		console.log(`Done routing all inputs to device ${selected}`)
+	const route_input = (index) =>
+		exec("pactl list short sink-inputs", (err, stdout) => {
+			if (err) {
+				return console.log("An error occured retrieving sink-input list (/tmp/cao.stderr)")
+				err_wstream.write(err + "\n")
+			}
+			
+			const outputLines = stdout.split("\n")
 		
-	})	
+			//remove last empty line
+			outputLines.splice(-1, 1)
+		
+			const inputList = []
+			outputLines.forEach(line => {
+				const columns = line.split("\t")
+		
+				const id = columns[0]	
+				inputList.push(id)
+			})
+
+			inputList.forEach(id => {
+				exec(`pactl move-sink-input ${id} ${index}`, err => {
+					if (err) {
+						console.log(`An error occured moving sink-input ${id}`)
+						err_wstream.write(err + "\n")
+					}
+				})
+			})
+
+			console.log(`Done routing all inputs to sink ${index}`)
+			
+		})
+
+	const set_default = (index) =>
+		exec(`pactl set-default-sink ${index}`, (err, stdout) => {
+			if (err) {
+				console.log(`An error occured setting sink ${index} as default sink`);
+				err_wstream.write(err + "\n")
+			} else console.log(`Sink ${index} set as default`);
+		})
 
 })
